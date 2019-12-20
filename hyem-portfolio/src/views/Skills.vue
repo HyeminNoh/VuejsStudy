5645d991f16783e3d58f4f3c0199857812b98d45
HyeminNoh <shrmffkfk@naver.com>
2019-12-20 18:43:52 +0900

    레이아웃 조정, 아이콘 이미지 추가, 이미지 텍스트 오버레이 진행중

--- hyem-portfolio/src/views/Skills.vue
+++ hyem-portfolio/src/views/Skills.vue
@@ -6,10 +6,82 @@
                 <p style="font-size: 10px; font-family:comfortaa; color: #ff4d5a;">CODING</p> 
             </div>
             <div class="chart-container">
-                <canvas id="codingChart" width="300" height="300"></canvas>
+                <canvas id="codingChart" width="330" height="330"></canvas>
             </div>
         </div>
         <div class="skills-detail">
+            <div class="skill-line">
+                <div class="language-icon">
+                    <img class="icon-img" align="center" src="../assets/img/html-logo.png"/>
+                    <div class="icon-overlay">
+                        <div class="text">html5</div>
+                    </div>
+                </div>
+                <div class="language-icon">
+                    <img class="icon-img" align="center" src="../assets/img/css-logo.png"/>
+                    <div class="icon-overlay">
+                        <div class="text">css3</div>
+                    </div>
+                </div>
+                <div class="language-icon">
+                    <img class="icon-img" align="center" src="../assets/img/js-logo.png"/>
+                    <div class="icon-overlay">
+                        <div class="text">javascript</div>
+                    </div>
+                </div>
+                <div class="language-icon">
+                    <img class="icon-img" align="center" src="../assets/img/bootstrap-logo.png"/>
+                    <div class="icon-overlay">
+                        <div class="text">bootstrap</div>
+                    </div>
+                </div>
+            </div>
+            <div class="skill-line">
+                <div class="language-icon">
+                    <img class="icon-img" align="center" src="../assets/img/php-logo.png"/>
+                    <div class="icon-overlay">
+                        <div class="text">php</div>
+                    </div>
+                </div>
+                <div class="language-icon">
+                    <img class="icon-img" align="center" src="../assets/img/vue-logo.png"/>
+                    <div class="icon-overlay">
+                        <div class="text">vue.js</div>
+                    </div>
+                </div>
+                <div class="language-icon">
+                    <img class="icon-img" align="center" src="../assets/img/node-logo.png"/>
+                    <div class="icon-overlay">
+                        <div class="text">node.js</div>
+                    </div>
+                </div>
+            </div>
+            <div class="skill-line">
+                <div class="language-icon">
+                    <img class="icon-img" align="center" src="../assets/img/mysql-logo.png"/>
+                    <div class="icon-overlay">
+                        <div class="text">mywql</div>
+                    </div>
+                </div>
+                <div class="language-icon">
+                    <img class="icon-img" align="center" src="../assets/img/c-logo.png"/>
+                    <div class="icon-overlay">
+                        <div class="text">c</div>
+                    </div>
+                </div>
+                <div class="language-icon">
+                    <img class="icon-img" align="center" src="../assets/img/java-logo.png"/>
+                    <div class="icon-overlay">
+                        <div class="text">java</div>
+                    </div>
+                </div>
+                <div class="language-icon">
+                    <img class="icon-img" align="center" src="../assets/img/python-logo.png"/>
+                    <div class="icon-overlay">
+                        <div class="text">python</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -32,10 +104,10 @@
         new Chart(ctx, {
             type: 'radar',
             data: {
-            labels: ["Vue.js", "Html", "Css", "Sass", "Bootstrap" ,["JavaScript", "      ES6"], "React.js", "Angular 6","Node.js","Express.js","MongoDB","Php","Sql","Wordpress","APIs",["Web   ","Sockets"],"Git", "C", "Java", "Linux"],
+            labels: ["Vue.js", "Html", "Css", "Bootstrap" ,["JavaScript", "      ES6"], "Node.js","Express.js","Php","Sql","APIs","Git", "C", "Java", "Python", "Linux"],
             datasets: [{
                 label: '# of Votes',
-                data: [4.5, 4.9, 4.9, 4.5, 4.9, 4.9, 4, 4, 4, 4, 4, 4, 3.5,4,3.5,3.5,4,4,4.5,4],
+                data: [4, 5, 4.5, 4, 4, 4, 3.5, 3, 4.5, 3.5, 4.5, 5, 4, 4.5, 3.5],
                 backgroundColor: [
                 'rgba(253,121,131,0.3)',
                 ],
@@ -69,27 +141,82 @@
                 }
             }
         });
+
     }
   }
 </script>
 
-<style>
+<style lang="scss">
  @import url("https://use.fontawesome.com/releases/v5.0.13/css/all.css");
 .skills-content{  
   position: absolute;
-  top: 56%;
-  left: 25%;
+  top: 55%;
+  left: 27%;
   transform: translate(-50%, -50%);
   text-align: center;
   font-family:comfortaa;
   }
 .skills-header{ 
-    margin-bottom: -30px;
+    margin-bottom: -20px;
+    margin-left: -40px;
 }
 .chart-container{
-  width: 480px;
+  width: 370px;
   margin:auto;
   font-family:comfortaa;
+}
+.skills-detail{
+    position: absolute;
+    top: 55%;
+    left: 68%;
+    transform: translate(-50%, -50%);
+    text-align: left;
+    font-family:comfortaa;
+}
+.skill-line{
+    width: 500px;
+    display: block;
+}
+.language-icon{
+    width: 110px;
+    height: 110px;
+    border: 1px solid black;
+    border-radius: 10px;
+    margin: 7px;
+    float: left;
+    text-align: center;
+}
+.icon-img{
+    display: block;
+    width: 100%;
+    height: auto;
+    padding: 10px;
+}
+.icon-overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 120px;
+  height: 120px;
+  margin: 5px;
+  border-radius: 10px;
+  opacity: 0;
+  transition: .5s ease;
+  background-color:#ff4d5a;
+}
+.language-icon:hover .icon-overlay {
+  opacity: 0.8;
+}
+.text {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
 }
 @media(max-width:1069px){
   .skills-content{
