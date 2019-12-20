c163270a8bddeb193ad02b9baa3f2b582276ca9f
HyeminNoh <shrmffkfk@naver.com>
2019-12-21 05:10:31 +0900

    cors설정파일

---
+++ hyem-portfolio/vue.cofing.js
@@ -0,0 +1,11 @@
+'use strict' 
+module.exports = { 
+    devServer: { 
+        proxy: { 
+            // proxyTable 설정 
+            '/api': { 
+                target: 'http://localhost:8080', changeOrigin: true 
+            } 
+        } 
+    } 
+}
