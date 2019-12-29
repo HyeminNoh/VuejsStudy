d340ad095be723db94c665ef85e09da0f6f75c84
HyeminNoh <shrmffkfk@naver.com>
2019-12-30 03:06:51 +0900

    파이어베이스 모듈 추가

--- hyem-portfolio/src/main.js
+++ hyem-portfolio/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueRouter from 'vue-router'
+import { rtdbPlugin } from 'vuefire'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(VueRouter);
+Vue.use(rtdbPlugin)
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
