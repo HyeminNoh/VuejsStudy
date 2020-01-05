d9fca1912c170e37cf9ef40e2ed6c5543c8b8cf3
HyeminNoh <shrmffkfk@naver.com>
2020-01-05 18:34:07 +0900

    rtdb로 부터 불러온 내용을 파싱해서 보여주는 프로젝트 내역 리스트 생성

---
+++ hyem-portfolio/src/views/ProjectList.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul class="project-list">
+    <li 
+      v-for="(item, index) in projects" 
+      v-bind:key="index" 
+      v-bind:class="['project-item']">
+        <a v-bind:href=item.Git target="_blank"><img v-bind:src=item.imageUrl v-bind:alt=index></a>
+        <div class="project-detail">
+            <a v-bind:href=item.Git target="_blank"><h5>{{index}}</h5></a>
+        </div>
+    </li>       
+  </ul>
+</template>
+<script>
+  export default {
+    props: ["projects"]
+  }
+</script>
+
+<style>
+.project-list{
+    width: 1000px;
+}
+.project-item{
+  list-style: none;
+  width: 300px;
+  position: relative;
+  margin: 7px;
+  float: left;
+  text-align: center;
+}
+.project-item > a > img{ 
+    display: block;
+    width: 100%;
+    height: auto;
+    border: 1px solid black;
+}
+.project-detail{
+  position: relative;
+  width: 300px;
+  margin-top: 10px;
+  text-align:center;
+}
+.project-detail>a{
+    color: black;
+}
+.project-detail>a:hover {
+  color: #ff4d5a;
+}
+</style>
